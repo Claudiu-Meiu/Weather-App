07a1e5751d5713d9d018db786acfbfbaa572e5da
Claudiu <claudiu-meiu@proton.me>
2025-06-19 14:59:23 +0300

    Added footer section; Added license for weather-icons; Updated readme; Styling adjustments.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 import { ToolbarComponent } from './components/layout/toolbar/toolbar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [CommonModule, RouterOutlet, ToolbarComponent],
+  imports: [CommonModule, RouterOutlet, ToolbarComponent, FooterComponent],
 })
 export class AppComponent {}
---
+++ src/app/components/layout/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ThemeService } from '../../../services/theme.service';
+
+@Component({
+  selector: 'app-footer',
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+})
+export class FooterComponent {
+  public themeService = inject(ThemeService);
+}
